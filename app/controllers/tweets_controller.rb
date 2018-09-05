b340ec8cd7f05aedb5aa298c6da99976c013d509
Ariel Zhen <zhenpingping@live.com>
2018-09-05 06:35:35 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -30,7 +30,16 @@ class TweetsController < ApplicationController
     end 
   end
 
-  get '/tweets/edit' do
+  get '/tweets/:id' do #tweets show page
+    if logged_in?
+      @tweet = Tweet.find_by_id(params[:id])
+      erb :'tweets/show_tweet'
+    else
+      redirect to '/login'
+    end
+  end
+
+  get '/tweets/:id/edit' do
     if logged_in?
       @user = current_user
       erb :'/tweets/edit'

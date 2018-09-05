160e5ff72b75e7861a97e17916b97e6a3df9ef5d
Ariel Zhen <zhenpingping@live.com>
2018-09-05 05:48:20 +0000

    automatically backed up by learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -3,5 +3,13 @@ class TweetsController < ApplicationController
   get '/tweets' do
     erb :'/tweets/index'
   end
+
+  get '/tweets/new' do 
+    erb :'/tweets/new' 
+  end 
+
+  post '/tweets' do 
+
+  end 
   
 end
